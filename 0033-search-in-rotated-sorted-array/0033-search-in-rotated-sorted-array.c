145230f97ccf051df6a0bf211b683acdc8363315
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-08 20:51:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (59.59%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.c
@@ -0,0 +1,17 @@
+int search(int* nums, int numsSize, int target) {
+ int n=numsSize;
+ int i,a,f;
+ for(i=0;i<n;i++)
+ {
+    if(target==nums[i])
+    {
+        a=i;
+        break;
+    }
+    else
+    {
+        a=-1;
+    }
+ }   
+return a;
+}
