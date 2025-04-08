e8d4b20556258b38b377d5d4faa907750efe75e3
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-08 20:56:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.16%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -0,0 +1,14 @@
+bool searchMatrix(int** matrix, int r, int* c, int target) {
+     int i,j;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<*c;j++)
+        {
+            if(matrix[i][j]==target)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
