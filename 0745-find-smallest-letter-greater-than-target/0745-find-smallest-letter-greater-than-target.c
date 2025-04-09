8a67940622a46570d25ca9d67554f9dbb4328ea6
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-09 21:15:15 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.12%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.c
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.c
@@ -1,3 +1,17 @@
 char nextGreatestLetter(char* letters, int lettersSize, char target) {
-    
-}
+    int n=lettersSize;
+    char a;
+    for(int i=0;i<n;i++)
+    {
+        if(letters[i]>target)
+        {
+            a=letters[i];
+            break;
+        }
+        else
+        {
+            a=letters[0];
+        }
+    }
+    return a;
+}   
