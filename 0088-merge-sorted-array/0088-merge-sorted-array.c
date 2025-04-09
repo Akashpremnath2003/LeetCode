4d08ab1796c183ed9c08a767a4714f54c788f398
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-09 21:00:04 +0530

    Time: 0 ms (100%), Space: 10.1 MB (27.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,21 +1,25 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) 
 {
-    int i,j,nums3[m+n],c=0;
-    for(i=m;i<m+n;i++)
+    int i=m-1; 
+    int j=n-1; 
+    int k=m+n-1; 
+    while (i>=0&&j>=0) 
     {
-        nums1[i]=nums2[c++];
-    }
-    for(i=0;i<m+n-1;i++)
-    {
-        for(j=i+1;j<n+m;j++)
+        if (nums1[i]>nums2[j]) 
         {
-            if(nums1[i]>nums1[j])
-            {
-            int c1=nums1[i];
-            nums1[i]=nums1[j];
-            nums1[j]=c1;
-            }
+            nums1[k]=nums1[i];
+            i--;
+        } else 
+        {
+            nums1[k]=nums2[j];
+            j--;
         }
+        k--;
     }
-
+    while (j>=0) 
+    {
+        nums1[k]=nums2[j];
+        j--;
+        k--;
+    }
 }
