2558097236cc5bba8dc12cec8afa5fbcf3c92a3c
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-10 11:17:55 +0530

    Time: 0 ms (100%), Space: 9.1 MB (23.53%) - LeetHub

--- 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.c
+++ 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.c
@@ -1,23 +1,11 @@
 int smallestEqual(int* nums, int numsSize) {
     int n=numsSize;
-    int i,a,b[n],c=0;
+    int i,a;
     for(i=0;i<n;i++)
     {
-        a=i%10;
-        if(a==nums[i])
-        {
-            b[c++]=i;
-        }
-    }   
-    if(c==0)
-    {
-        return -1;
+        if(i%10==nums[i])
+            return i;
     }
-    int s=b[0];
-    for(i=0;i<c;i++)
-    {
-        if(b[i]<s)
-            s=b[i];
-    }
-    return s;
+   
+    return -1;
 }
