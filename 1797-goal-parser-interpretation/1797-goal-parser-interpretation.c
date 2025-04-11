5f4181abebf2f6eddc9434401fd7ccaac2fa8225
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-11 17:39:12 +0530

    Time: 3 ms (49.21%), Space: 8.8 MB (3.17%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.c
@@ -0,0 +1,28 @@
+
+
+char * interpret(char * s){
+    int n=strlen(s);
+    char *a=malloc(10000*sizeof(char));
+    int i,c=0;
+    for(i=0;i<n;i++)
+    {
+        if(s[i]=='G')
+        {
+            a[c++]='G';
+        }
+        if(s[i]=='('&&s[i+1]==')')
+        {
+            a[c++]='o';
+        }
+        if(s[i]=='('&& s[i+1]=='a'&&s[i+2]=='l'&&s[i+3]==')')
+        {
+            a[c++]='a';
+            a[c++]='l';
+        }
+
+            
+    }
+    
+    a[c]='\0';
+return a;
+}
