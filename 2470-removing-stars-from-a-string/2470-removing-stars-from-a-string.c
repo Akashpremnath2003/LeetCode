730e8fc9a0a4a6e60fd1622db01bb7c1f7bf7c94
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-04-11 18:59:05 +0530

    Time: 4 ms (79.21%), Space: 14.1 MB (56.36%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.c
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.c
@@ -1,18 +1,12 @@
 char* removeStars(char* s) {
     int n=strlen(s);
-    int i,j,c=0;
+    int c=0;
     char *a=(char *)malloc(n+1*sizeof(char));
-    for(i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++)
         if(s[i]!='*')
-        {
             a[c++]=s[i];
-        }
         else
-        {
             c--;
-        }
-    }
   a[c]='\0';
     return a;
 }
