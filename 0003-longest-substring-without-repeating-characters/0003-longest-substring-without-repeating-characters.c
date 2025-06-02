3388cf11d1939e5cd672ef3818894899622ee4e1
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-06-02 12:05:00 +0530

    Time: 0 ms (100%), Space: 8.8 MB (87.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -1,29 +1,22 @@
-int lengthOfLongestSubstring(char* s) {
-    int n=strlen(s);
-    if(n == 0)
-     return 0;
-    int max = 0;
-     for(int i=0;i<n;i++)
-     {char *a= (char*) malloc((n+1)*sizeof(char));
-        int k=0;
-        for(int j=i;j<n;j++)
-        {   int f = 0;
-            for(int l=0;l<k;l++)
-            if(s[j] == a[l])
-            {f=1;
-             break;
+int lengthOfLongestSubstring(char* a) {
+    int n = strlen(a);
+    int maxLen = 0;
+    int start = 0; // Start index of current window
+    int i, j;
+
+    for (i = 0; i < n; i++) {
+        for (j = start; j < i; j++) {
+            if (a[i] == a[j]) {
+                start = j + 1;  // Move start after the previous occurrence
+                break;
             }
-            if(f == 0)
-            { a[k] = s[j];
-                k++;
-            }
-            else
-                break; 
         }
-        a[k] = '\0';
-        k++;
-        if(strlen(a) > max)
-        max = strlen(a);
+
+        // This must be outside the inner loop
+        int len = i - start + 1;
+        if (len > maxLen)
+            maxLen = len;
     }
-    return max;
+
+    return maxLen;
 }
