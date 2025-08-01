8d83673c17d7ae60b47e70d162a232774d053d66
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-08-01 14:32:34 +0530

    Time: 5 ms (9.95%), Space: 41.8 MB (45.5%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         String b=sentence.toLowerCase();
         char a[]=b.toCharArray();
-        Set<Character> c=new TreeSet<>();
+        Set<Character> c=new HashSet<>();
         for(char i:a)
             c.add(i);
         if(c.size()==26)
