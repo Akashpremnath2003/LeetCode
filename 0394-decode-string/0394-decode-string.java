646e45decf83b3ae90571d10590f95384e77778c
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-08-29 13:40:40 +0530

    Time: 6 ms (12.11%), Space: 42.2 MB (7.97%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,37 +1,36 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> in=new Stack<>();
-        Stack<String> st=new Stack<>();
-        int sum=0;
+        Stack<Integer> num=new Stack<>();
+        Stack<String> str=new Stack<>();
+        int s2=0;
         String s1="";
-        for(char j:s.toCharArray())
+        for(int i=0;i<s.length();i++)
         {
-            if(Character.isDigit(j))
+            if(Character.isDigit(s.charAt(i)))
             {
-                sum=(sum*10)+(j-'0');
+                s2=(s2*10)+(s.charAt(i)-'0');
             }
-            else if(j=='[')
+            else if(s.charAt(i)=='[')
             {
-                in.push(sum);
-                sum=0;
-                st.push(s1);
+                num.push(s2);
+                s2=0;
+                str.push(s1);
                 s1="";
             }
-            else if(j==']')
+            else if(s.charAt(i)==']')
             {
-                int k=in.pop();
-                String ans=s1;
+                int k=num.pop();
+                String t=s1;
                 s1="";
-                for(int i=0;i<k;i++)
+                for(int j=0;j<k;j++)
                 {
-                    s1+=ans;
+                    s1+=t;
                 }
-                s1=st.pop()+s1;
-
+                s1=str.pop()+s1;
             }
             else
             {
-                s1=s1+j;
+                s1=s1+s.charAt(i);
             }
 
         }
