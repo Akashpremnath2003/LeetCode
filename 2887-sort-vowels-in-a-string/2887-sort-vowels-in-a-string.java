de1369d67b61d5a85bf14070ed251c11898e29f5
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-09-11 18:24:39 +0530

    Time: 843 ms (14.58%), Space: 47.5 MB (50.7%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,18 +1,15 @@
 class Solution {
     public String sortVowels(String s) {
         char a[]=s.toCharArray();
-        List<Character> c=new ArrayList<>();
+        String s1="";
         for(char k:a)
         {
             if(k=='a'||k=='A'||k=='e'||k=='E'||k=='i'||k=='I'||k=='o'||k=='O'||k=='u'||k=='U')
-            c.add(k);
+            s1=s1+k;
         }
-        char b[]=new char[c.size()];
-        int c1=0;
-        for(char x:c)
-            b[c1++]=x;
+        char b[]=s1.toCharArray();
         Arrays.sort(b);
-        c1=0;
+        int c1=0;
         for(int i=0;i<s.length();i++)
         {
             if(a[i]=='a'||a[i]=='A'||a[i]=='e'||a[i]=='E'||a[i]=='i'||a[i]=='I'||a[i]=='o'||a[i]=='O'||a[i]=='u'||a[i]=='U')
