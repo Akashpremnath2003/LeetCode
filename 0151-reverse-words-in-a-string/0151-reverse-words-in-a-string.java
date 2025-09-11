dd2019a56b0c3fbed7f1cb304ea31645480379d0
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-09-11 11:52:32 +0530

    Time: 17 ms (6.62%), Space: 45.1 MB (18.78%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,14 +1,55 @@
 class Solution {
     public String reverseWords(String s) {
-      String a[]=s.split(" ");
-    String s1="";
-    System.out.println(a.length);
-      for(int i=a.length-1;i>=0;i--)
-    {
-        if(a[i]=="")
-            continue;
-            s1=s1+a[i]+" ";
-    }        
-      return s1.trim();
+        char a[]=s.toCharArray();
+        int l=0;
+		int r=s.length()-1;
+		while(l<r)
+		{
+			char c1=a[l];
+			a[l]=a[r];
+			a[r]=c1;
+			l++;
+			r--;
+		}
+
+		int j=0;
+		l=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(a[i]==' ')
+			{
+				r=i-1;
+				while(l<r)
+				{
+					char c1=a[l];
+					a[l]=a[r];
+					a[r]=c1;
+					l++;
+					r--;
+				}
+				l=i+1;
+				
+			}
+		}
+		r=s.length()-1;
+		while(l<r)
+		{
+			char c1=a[l];
+			a[l]=a[r];
+			a[r]=c1;
+			l++;
+			r--;
+		}
+        String ans="";
+		for(int i=0;i<a.length-1;i++)
+		{
+			if(a[i]==' '&&a[i+1]==' ')
+				continue;
+			ans=ans+a[i];
+		}
+        ans=ans+a[a.length-1];
+
+        
+        return ans.trim();
     }
 }
