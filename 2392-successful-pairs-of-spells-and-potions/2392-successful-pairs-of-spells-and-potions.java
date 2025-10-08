0c00c4a3f1c3f9fb743bd0ac3e967100c33684cc
Akashpremnath2003 <akashpremnath3@gmail.com>
2025-10-08 12:25:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,36 +1,19 @@
 class Solution {
-    public int[] successfulPairs(int[] spells, int[] a, long success) {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
         List<Integer> list=new ArrayList<>();
         int c=0;
-        Arrays.sort(a);
-        int ans=-1;
-        int m=0;
         for(int i:spells)
         {
-            int l=0;
-            int r=a.length-1;
-            ans=-1;
-            while(l<=r)
+            c=0;
+            for(int j:potions)
             {
-                m=(l+r)/2;
-                if((long)a[m]*i>=success)
-                {
-                    ans=m;
-                    r=m-1;
-                }
-                else
-                {
-                    l=m+1;
-                }
+                long x=(long)i*j;
+                if(x>=success)
+                    c++;
             }
-
-            if(ans>=0)
-                list.add(a.length-ans);
-            else
-                list.add(0);
-                
+            list.add(c);
         }
-        int ans1[]=list.stream().mapToInt(Integer::intValue).toArray();
-        return ans1;
+        int ans[]=list.stream().mapToInt(Integer::intValue).toArray();
+        return ans;
     }
 }
